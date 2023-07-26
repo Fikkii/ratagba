f7d7c7b1cd7a6b43d175c9a5e1cd4ce8def2a176
Fikkii <98499137+Fikkii@users.noreply.github.com>
2023-07-26 08:16:33 +0100

    Update db.js

--- db.js
+++ db.js
@@ -1,9 +1,8 @@
 const mongoose = require('mongoose');
 
-const connectDB = mongoose.connect('mongodb+srv://Fikkyart:mydadaskedmetoprepare@cluster0.2iaq2am.mongodb.net/', {
+mongoose.connect('mongodb+srv://Fikkyart:mydadaskedmetoprepare@cluster0.2iaq2am.mongodb.net/', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 }).then(() => console.log('conected to database successfully'))
   .catch(err => console.log('Unable to connect to database', err))
 
-module.exports = { connectDB }
