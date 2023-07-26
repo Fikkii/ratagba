e7e83c64d1f7a2f0a06d592c8649d896c30ddf46
Fikkii <98499137+Fikkii@users.noreply.github.com>
2023-07-26 08:14:07 +0100

    Update app.js

--- app.js
+++ app.js
@@ -34,4 +34,4 @@ app.use((req, res, next) => {
 });
 
 connectDB()
-  app.listen(3000, () => console.log('App started on port 3000'))}
+  app.listen(3000, () => console.log('App started on port 3000'))
